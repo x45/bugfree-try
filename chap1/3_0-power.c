ee5c593eac7b09b0f673aa63c3c28036c297f7f3
x45 <0x45678@zoho.com>
2013-07-20 16:03:08 +0200

    Chap 1

---
+++ chap1/3_0-power.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int power(int m, int n);
+
+int main (int argc, char const* argv[])
+{
+	int i;
+
+	for (i = 0; i < 10; ++i)
+		printf("%d %d %d\n",i ,power(2,i), power(-3,i));
+	return 0;
+}
+
+int power (int base, int n)
+{
+	int i, p = 1;
+	for (i = 1; i <= n; ++i)
+		p *= base;
+	return p;
+}
