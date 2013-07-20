b551baa2b1a2f58c29cc0664b574c01ff57e52eb
x45 <0x45678@zoho.com>
2013-07-20 15:47:40 +0200

    Chap 1

--- chap1/2_9-countw.c
+++ chap1/2_9-countw.c
@@ -1,3 +1,23 @@
 #include <stdio.h>
 
+#define MAX 93
 
+int main (int argc, char const* argv[])
+{
+	int c, i, count[MAX]= { 0 };
+	
+
+	while ((c = getchar()) != EOF){
+		if (c >=30 && c <= 122)
+			++count[(c-30)];
+		else if (c == '\n')
+			break;
+	}
+
+	printf("Case\t|\tOccurances\n");
+	for (i = 0; i < MAX; ++i){
+		printf("%c\t|\t%d\n",(i+30),count[i]);
+	}
+
+
+}
