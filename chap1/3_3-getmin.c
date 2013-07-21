5864970d9a15248a8a5b6306617493ddbbc597b3
x45 <0x45678@zoho.com>
2013-07-21 02:32:13 +0200

    Chap 1

---
+++ chap1/3_3-getmin.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define MINLINE 80
+
+int get_line(char line[], int minline);
+void copy(char to[], char from[]);
+
+int main (int argc, char const* argv[])
+{
+	return 0;
+}
+int get_line(char s[], int lim)
+{
+	//
+}
+
+void copy(char to[], char from[])
+{
+	int i=0;
+
+	while ((to[i] = from[i]) != '\0')
+		++i;
+}
