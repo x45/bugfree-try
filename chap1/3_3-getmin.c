5dc5edb4d1e5303d82219de130150d08d5b1b2ed
x45 <0x45678@zoho.com>
2013-07-21 08:14:23 +0200

    Chap 1

--- chap1/3_3-getmin.c
+++ chap1/3_3-getmin.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 
-#define MINLINE 80
+#define MAXLINE 1000
+#define MINCHAR 8
 
-int get_line(char line[], int minline);
-void copy(char to[], char from[]);
+int get_line(char line[],int maxline);
 
 int main (int argc, char const* argv[])
 {
+	int len, max = 0;
+	char line[MAXLINE];
+
+	while ((len = get_line(line,MAXLINE)) > 0)
+		if (len > MINCHAR) {
+			printf("%s",line);
+		}
 	return 0;
 }
 int get_line(char s[], int lim)
 {
-	//
-}
+	int c, i;
 
-void copy(char to[], char from[])
-{
-	int i=0;
-
-	while ((to[i] = from[i]) != '\0')
+	for (i=0;i < lim-1 &&(c=getchar()) != EOF && c != '\n' ;++i)
+		s[i] = c;
+	if (c == '\n') {
+		s[i] = c;
 		++i;
+	}
+	s[i] = '\0';
+	return i;
 }
+
