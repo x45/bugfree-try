af357261bc14c8bf418a6e70fbea42677b29ec05
x45 <0x45678@zoho.com>
2013-07-25 10:09:40 +0200

    Chap 2

---
+++ chap2/1_2-for.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+for (i=0; i < lim-1 && (c=getchar()) != '\n' && c != EOF; ++i)
+	s[i] = c;
+
+i = 0;
+while (i < lim-1){
+	if ((c =getchar ()) != '\n')
+		if (c != EOF)
+			s[î] = c;
+	i++;
+}
