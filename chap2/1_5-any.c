957ba7fd67b31ab64c44b4066570865cdbca37c7
x45 <0x45678@zoho.com>
2013-07-26 09:22:41 +0200

    Chap 2

--- chap2/1_5-any.c
+++ chap2/1_5-any.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
-void any(void);
+int any(char s1[],char s2[]);
 
 int main (int argc, char const* argv[])
 {
-	any();
+	int inde;	
+	inde = any("tommy","jerry");
+	printf("%d\n",inde);
 	return 0;
 }
 
-void any(void)
+int any(char s1[], char s2[])
 {
-	return ;
+	int i, j;
+	for (i = 0; s1[i] != '\0'; ++i)
+		for (j = 0; s2[j] != '\0'; ++j)
+			if (s1[j] == s2[i])
+				return i;
+	return -1;
+		
+	
 }
 
