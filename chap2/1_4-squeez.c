f5b61aeb82dbeb7ad45dbf46cffcc8f7174f9abb
x45 <0x45678@zoho.com>
2013-07-26 08:34:22 +0200

    Chap 2

--- chap2/1_4-squeez.c
+++ chap2/1_4-squeez.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 
+void squeez(char s1[], char s2[]);
+
 int main (int argc, char const* argv[])
 {
-	
-	return 0;
+    	char s1[] = "Time to put the squeeze on!\n";
+        char s2[] = "ez";
+
+	printf("%s", s1);
+	squeez(s1,s2);
+	printf("%s", s1);
 }
+
+
+void squeez(char s1[], char s2[])
+{
+	int i, j, k;
+	for (i = 0; s2[i] != '\0'; ++i){
+		for (j = k = 0; s1[j] != '\0'; ++j){
+			if (s1[j] != s2[i])
+				s1[k++] = s1[j];
+		}
+		s1[k++] = '\0';	
+	}
+}
+
+
 
