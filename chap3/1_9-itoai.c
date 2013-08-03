576ba23e7f4f462f6bb75a5eb4f8accafd431cf0
x45 <0x45678@zoho.com>
2013-08-03 17:50:30 +0200

    Chap 3

--- chap3/1_9-itoai.c
+++ chap3/1_9-itoai.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <limits.h>
 #include <string.h>
 
 void reverse(char s[]);
@@ -7,10 +8,11 @@ void itoa(int n, char s[]);
 
 int main (int argc, char const* argv[])
 {
-	char s[] = "It\'s an input lol";
-	printf("%s\n",s);
-	reverse(s);
-	printf("%s\n",s);
+	int num = INT_MIN;
+	char str[10];
+	printf("Int : %d \n", num);
+	itoa(num,str);
+	printf("Str : %s \n", str);
 	return 0;
 }
 
@@ -18,22 +20,27 @@ void reverse(char s[])
 {
 	int c, i, j;
 
-	for (i = 0 , j = strlen(s)-1; i < j; i++, j--){
+	for (i = 0, j = strlen(s)-1; i<j; i++, j--){
 		c = s[i];
 		s[i] = s[j];
 		s[j] = c;
 	}
 }
+
+
 void itoa(int n, char s[])
 {
 	int i, sign;
+	long long int num = n;
 
-	if ((sign = n) < 0)
-		n= -n;
+	if (( sign = num) < 0)
+		num = -num;
+
 	i = 0;
+
 	do {
-		s[i++] = n % 10 + '0';
-	} while ((n /= 10) > 0);
+		s[i++] = num % 10 + '0';
+	}while ((num /= 10) > 0);
 	if (sign < 0)
 		s[i++] = '-';
 	s[i] = '\0';
