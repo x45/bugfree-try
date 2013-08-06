881f05bc2465a84840ffe944f79301942aee9f43
x45 <0x45678@zoho.com>
2013-08-07 00:19:33 +0200

    Chap 4

--- chap4/1_5-calcp.c
+++ chap4/1_5-calcp.c
@@ -1,133 +1,223 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<ctype.h>
+#include<math.h>
+#include <string.h>
 
-#define MAXOP 		100
-#define NUMBER 		'0'
+#define MAXOP 100
+#define NUMBER       0
+#define IDENTIFIER   1
+#define TRUE 1
+#define FALSE 0
 
-int getop(char []);
-void push(double);
-double pop (void);
+int Getop(char s[]);
+void push(double val);
+double pop(void);
+void showTop(void);
+void duplicate(void);
+void swapItems(void);
+void clearStack();
+void dealWithName(char s[]);
 
 int main(int argc, const char *argv[])
 {
-	int type;
-	double op2;
-	char s[MAXOP];
-
-	while (( type = getop(s)) != EOF) {
-			switch (type) {
-			case NUMBER:
-					push(atof(s));
-					break;
-			case '+':
-					push(pop() + pop());
-					break;
-			case '*':
-					push(pop() * pop());
-					break;
-			case '-':
-					op2 = pop();
-					push(pop() - op2);
-					break;
-			case '%':
-					op2 = pop();
-					if (op2 != 0.0)
-							push((int) pop() % (int) op2 );
-					else
-							printf("Error: zero divisor\n");
-					break;
-			case '/':
-					op2 = pop();
-					if (op2 != 0.0)
-							push (pop() / op2);
-					else
-							printf("Error: zero divisor\n");
-					break;
-					case '\n':
-						printf("\t%.8g\n",pop());
-						break;
-					default:
-						printf("Error: unknown cmd %s\n",s);
-						break;
-			}
-	}
-
-	return 0;
+   int type;
+   double op2;
+   char s[MAXOP];
+   
+   while((type = Getop(s)) != EOF)
+   {
+      switch(type)
+      {
+      case NUMBER:
+         push(atof(s));
+         break;
+      case IDENTIFIER:
+         dealWithName(s);
+         break;
+      case '+':
+         push(pop() + pop());
+         break;
+      case '*':
+         push(pop() * pop());
+         break;
+      case '-':
+         op2 = pop();
+         push(pop()- op2);
+         break;
+      case '/':
+         op2 = pop();
+         if(op2)
+            push(pop() / op2);
+         else
+            printf("\nError: division by zero!");
+         break;
+      case '%':
+         op2 = pop();
+         if(op2)
+            push(fmod(pop(), op2));
+         else
+            printf("\nError: division by zero!");
+         break;
+      case '?':
+         showTop();
+         break;
+      case '#':
+         duplicate();
+         break;
+      case '~':
+         swapItems();
+         break;
+      case '!':
+         clearStack();
+      case '\n':
+         printf("\n\t%.8g\n", pop());
+         break;
+      default:
+         printf("\nError: unknown command %s.\n", s);
+         break;
+      }
+   }
+   return EXIT_SUCCESS;
 }
 
-#define MAXVAL 		100
+#define MAXVAL 100
 
-int sp = 0;
+int sp = 0;       
 double val[MAXVAL];
 
 void push(double f)
 {
-	if (sp < MAXVAL)
-		val[sp++] = f;
-	else
-		printf("Stack full :  %g\n",f);
+   if(sp < MAXVAL)
+      val[sp++] = f;
+   else
+      printf("\nError: stack full can't push %g\n", f);
 }
 
 double pop(void)
 {
-	if (sp > 0)
-		return val[--sp];
-	else {
-		printf("Stack empty\n");
-		return 0.0;
-	}
+   if(sp > 0)
+      return val[--sp];
+   else
+   {
+      printf("\nError: stack empty\n");
+      return 0.0;
+   }
 }
 
-#include <ctype.h>
+void showTop(void)
+{
+   if(sp > 0)
+      printf("Top of stack contains: %8g\n", val[sp-1]);
+   else
+      printf("The stack is empty!\n");
+}
+
+
+void duplicate(void)
+{
+   double temp = pop();
+   
+   push(temp);
+   push(temp);
+}
+
+void swapItems(void)
+{
+   double item1 = pop();
+   double item2 = pop();
+   
+   push(item1);
+   push(item2);
+}
+
+void clearStack(void)
+{
+   sp = 0;
+}
+
+void dealWithName(char s[])
+{
+   double op2;
+   
+   if( 0 == strcmp(s, "sin"))
+      push(sin(pop()));
+   else if( 0 == strcmp(s, "cos"))
+      push(cos(pop()));
+   else if (0 == strcmp(s, "exp"))
+      push(exp(pop()));
+   else if(!strcmp(s, "pow"))
+   {
+      op2 = pop();
+      push(pow(pop(), op2));
+   }
+   else
+      printf("%s is not a supported function.\n", s);
+}
 
 int getch(void);
-void ungetch(int);
+void unGetch(int);
 
-int getop(char s[])
+int Getop(char s[])
 {
-	int i, c;
-
-	while ((s[0] = c = getch()) == ' ' || c == '\t')
-			;
-	s[1] = '\0';
-	if (!isdigit(c) && c != '.'&& c != '-')
-		return c;
-	i = 0;
-	if(c == '-') {
-		if (isdigit(c = getch()) || c == '.')
-				s[++i] = c;
-		else {
-				if (c != EOF)
-						ungetch(c);
-				return '-';
-		}
-	}
-	if (isdigit(c))
-		while (isdigit(s[++i] = c = getch()))
-			;
-	if (c == '.')
-		while (isdigit(s[++i] = c = getch()))
-			;
-	s[i] = '\0';
-	if (c != EOF)
-		ungetch(c);
-	return NUMBER;
+   int i = 0;
+   int c;
+   int next;
+   while((s[0] = c = getch()) == ' ' || c == '\t')
+      ;
+   s[1] = '\0';
+   
+   if(isalpha(c))
+   {
+      i = 0;
+      while(isalpha(s[i++] = c ))
+         c = getch();     
+      s[i - 1] = '\0';
+      if(c != EOF)
+         unGetch(c);
+      return IDENTIFIER;
+   }
+   
+   if(!isdigit(c) && c != '.' && c != '-')
+      return c;                 
+   
+   if(c == '-')
+   {
+      next = getch();
+      if(!isdigit(next) && next != '.')
+      {
+         return c;
+      }
+      c = next;
+   }
+   else
+      c = getch();
+   
+   while(isdigit(s[++i] = c))
+      c = getch();
+   if(c == '.')                 
+      while(isdigit(s[++i] = c = getch()))
+         ;
+      s[i] = '\0';
+      if(c != EOF)
+         unGetch(c);
+      return NUMBER;
 }
 
-#define BUFFSIZE 100
+#define BUFSIZE 100
 
-char buf[BUFFSIZE];
+char buf[BUFSIZE];
 int bufp = 0;
 
 int getch(void)
 {
-	return (bufp > 0) ? buf[--bufp] : getchar();
+   return (bufp > 0) ? buf[--bufp]: getchar();
 }
 
-void ungetch(int c)
+void unGetch(int c)
 {
-	if (bufp >= BUFFSIZE)
-		printf("Too many chars \n");
-	else
-		buf[bufp++] = c;
+   if(bufp >= BUFSIZE)
+      printf("\nUnGetch: too many characters\n");
+   else
+      buf[bufp++] = c;
 }
