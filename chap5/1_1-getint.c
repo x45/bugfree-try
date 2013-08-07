1733e1730313280ba88715ba5d3246a8c984982c
x45 <0x45678@zoho.com>
2013-08-07 23:22:22 +0200

    Chap 5

---
+++ chap5/1_1-getint.c
@@ -0,0 +1,44 @@
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+int getint(int *pn)
+{
+	int c, sign;
+
+	while (isspace(c=getch()))
+			;
+	if (!isdigit(c) && c != EOF && c!= '+' && c!= '-'){
+		ungetch(c);
+		return 0;
+	}
+	sign = (c =='-') ? -1: 1;
+	if (c == '+' || c == '-')
+			c = getch();
+	for (*pn = 0; isdigit(c); c=getch())
+			*pn = 10 * *pn + (c - '0');
+	*pn *= sign;
+	if (c != EOF)
+			ungetch(c);
+	return c;
+}
+
+#define BUFFSIZE 100
+
+char buf[BUFFSIZE];
+int bufp = 0;
+
+int getch(void)
+{
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+	if (bufp >= BUFFSIZE)
+			printf("Buffer Full\n");
+	else
+			buf[bufp++] = c;
+}
+
