0a97b4ceca056070de5ffbf35681f7f78ac87894
x45 <0x45678@zoho.com>
2013-08-08 14:23:09 +0200

    Chap 5

--- chap5/1_1-getint.c
+++ chap5/1_1-getint.c
@@ -5,7 +5,7 @@ void ungetch(int);
 
 int getint(int *pn)
 {
-	int c, sign;
+	int c, d, sign;
 
 	while (isspace(c=getch()))
 			;
@@ -14,8 +14,16 @@ int getint(int *pn)
 		return 0;
 	}
 	sign = (c =='-') ? -1: 1;
-	if (c == '+' || c == '-')
-			c = getch();
+	if (c == '+' || c == '-'){
+		c = d;
+		if (!isdigit(c = getch())){
+				if (c != EOF)
+					ungetch(c);
+				ungetch(d);
+				return d;
+		}
+	}
+
 	for (*pn = 0; isdigit(c); c=getch())
 			*pn = 10 * *pn + (c - '0');
 	*pn *= sign;
