4e71935117457269a6f904e658037e06bf81a5f1
x45 <0x45678@zoho.com>
2013-08-09 17:13:09 +0200

    Chap 5

---
+++ chap5/1_8-strcat.c
@@ -0,0 +1,17 @@
+void strcat(char s[], char t[])
+{
+	int i, j;
+	i = j = 0;
+	while( s[i] != '\0')
+			i++;
+	while ((s[i++] = t[j++]) != '\0')
+			;
+}
+
+void strcat(char *s, char *t)
+{
+	while( *++s ')
+			;
+	while (*s++ = *t++)
+			;
+}
