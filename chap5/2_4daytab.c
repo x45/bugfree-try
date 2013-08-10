04062ab0cd980111d36e04b7d89e1c156941db0f
x45 <0x45678@zoho.com>
2013-08-10 19:18:58 +0200

    Chap 5

--- chap5/2_4daytab.c
+++ chap5/2_4daytab.c
@@ -3,12 +3,11 @@ static char daytab[2][13]= {
 		{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 };
 
-int day_of_year(int year, int mont, int day)
+int day_of_year(int year, int month, int day)
 {
 	int i, leap;
 
 	leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
-	leap = yea%4 == 0 && y;
 	if (month < 1 || month > 12)
 			return -1;
 	if (day < 1 || day > daytab[leap][month])
