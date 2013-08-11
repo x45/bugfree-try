049a977d8b5426b95746aa761259e3f47abca574
x45 <0x45678@zoho.com>
2013-08-11 08:08:00 +0200

    Chap 5

--- chap5/3_1-polish.c
+++ chap5/3_1-polish.c
@@ -3,6 +3,7 @@
 #define NUMBER '0'
 #define MAX 100
 
+void un_gets(char []);
 int getop(char []);
 void push(double);
 double pop(void);
@@ -10,12 +11,13 @@ double pop(void);
 
 int main(int argc, const char *argv[])
 {
-	int type;
 	double op2;
 	char s[MAX];
 
-	while ((type = getop(s)) != EOF ) {
-			switch(type) {
+	while (--argc > 0) {
+			un_gets(" ");
+			un_gets(*++argv);
+			switch(getop(s)) {
 			case NUMBER:
 				push(atof(s));
 				break;
@@ -41,76 +43,11 @@ int main(int argc, const char *argv[])
 				break;
 			default:
 				printf("Error: unknown command %s\n",s);
+				argc = 1;
 				break;
 			}
 	}
+	printf("\t%.8g\n",pop());
 	return 0;
 }
 
-#define MAXVAL 100
-
-int sp = 0;
-double val[MAXVAL];
-
-void push(double f)
-{
-	if (sp < MAXVAL)
-		val[sp++] =f;
-	else
-		printf("Error full stack %g\n",f);
-}
-
-double pop(void)
-{
-	if (sp > 0)
-			return val[--sp];
-	else {
-		printf("Error stack empty");
-		return 0.0;
-	}
-}
-
-#include <ctype.h>
-
-int getch(void);
-void ungetch(int);
-
-int getop(char s[])
-{
-	int i, c;
-
-	while ((s[0] = c = getch()) == ' ' || c == '\t')
-		;
-	s[1] = '\0';
-	if (!isdigit(c) && c != '.')
-		return c;
-	i = 0;
-	if (isdigit(c))
-		while ( isdigit(s[++i] = c = getch()))
-			;
-	if (c == '.')
-		while (isdigit(s[++i] = c = getch()))
-			;
-	s[i] = '\0';
-	if (c != EOF)
-		ungetch(c);
-	return NUMBER;
-}
-
-#define BUFSIZE 100
-
-char buf[BUFSIZE];
-int bufp = 0;
-
-int getch(void)
-{
-	return (bufp > 0) ? buf[--bufp] : getchar();
-}
-
-void ungetch(int c)
-{
-	if (bufp >= BUFSIZE)
-		printf("Too many char\n");
-	else
-		buf[bufp++] = c;
-}
