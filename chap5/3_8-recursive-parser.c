7aa4bacf5cd7a0cd16b6e9e73db3c0011714dc3c
x45 <0x45678@zoho.com>
2013-08-12 11:23:53 +0200

    Chap 5

--- chap5/3_8-recursive-parser.c
+++ chap5/3_8-recursive-parser.c
@@ -24,12 +24,12 @@ char out[1000];
 int main(int argc, const char *argv[])
 {
 	while(gettoken() != EOF) {
-		strcpy(datatype, token);
-		out[0] = '\0';
-		dcl();
-		if(tokentype != '\n')
-			printf("syntax error\n");
-		printf("%s: %s %s",name, out, datatype);
+			strcpy(datatype, token);
+			out[0] = '\0';
+			dcl();
+			if(tokentype != '\n')
+					printf("syntax error\n");
+			printf("%s: %s %s",name, out, datatype);
 	}
 	return 0;
 }
@@ -41,28 +41,28 @@ int gettoken(void)
 	char *p = token;
 
 	while ((c = getch()) == ' ' || c == '\t')
-		;
-	if (c == '(') {
-		if ((c = getch()) == ')') {
-			strcpy(token, "()");
-			return tokentype = PARENS;
-		} else {
-			ungetch(c);
-			return tokentype = '(';
-		}
-	} else if (c == '[') {
-		for (*p++ = c; (*p++ = getch()) != ']'; )
 			;
-		*p = '\0';
-		return tokentype = BRACKETS;
-	} else if (isalpha(c)) {
-		for (*p++ = c; isalnum(c = getch()); )
-			*p++ = c;
-		*p = '\0';
-		ungetch(c);
-		return tokentype = NAME;
-	} else
-		return tokentype = c;
+	if (c == '(') {
+			if ((c = getch()) == ')') {
+					strcpy(token, "()");
+					return tokentype = PARENS;
+			} else {
+					ungetch(c);
+					return tokentype = '(';
+			}
+			} else if (c == '[') {
+					for (*p++ = c; (*p++ = getch()) != ']'; )
+							;
+					*p = '\0';
+					return tokentype = BRACKETS;
+			} else if (isalpha(c)) {
+					for (*p++ = c; isalnum(c = getch()); )
+							*p++ = c;
+					*p = '\0';
+					ungetch(c);
+					return tokentype = NAME;
+			} else
+					return tokentype = c;
 }
 
 void dcl(void)
@@ -70,10 +70,10 @@ void dcl(void)
 	int ns;
 
 	for (ns = 0; gettoken() == '*'; )
-		ns++;
+			ns++;
 	dirdcl();
 	while (ns-- > 0)
-		strcat(out, " pointer to");
+			strcat(out, " pointer to");
 }
 
 void dirdcl(void)
@@ -81,20 +81,20 @@ void dirdcl(void)
 	int type;
 
 	if (tokentype == '(') {
-		dcl();
-		if (tokentype != ')')
-			printf("Error: missing )\n");
-		} else if (tokentype == NAME)
-		strcpy(name, token);
+			dcl();
+			if (tokentype != ')')
+					printf("Error: missing )\n");
+			} else if (tokentype == NAME)
+					strcpy(name, token);
 	else
-		printf("Error: expected name or (dlc)\n");
+			printf("Error: expected name or (dlc)\n");
 	while ((type=gettoken()) == PARENS || type == BRACKETS)
-		if (type == PARENS)
-			strcat(out, "function returning");
-		else {
-			strcat(out, " array");
-			strcat(out, token);
-			strcat(out, " of");
+			if (type == PARENS)
+					strcat(out, " function returning");
+			else {
+					strcat(out, " array");
+					strcat(out, token);
+					strcat(out, " of");
 		}
 }
 
