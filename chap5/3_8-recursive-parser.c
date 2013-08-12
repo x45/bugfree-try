6eef0ffe72b1cf4793704748d95d0ec38cc31f17
x45 <0x45678@zoho.com>
2013-08-12 11:03:14 +0200

    Chap 5

--- chap5/3_8-recursive-parser.c
+++ chap5/3_8-recursive-parser.c
@@ -1,12 +1,79 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAXTOKEN 100
+#define BUFSIZE 100
+
+enum { NAME, PARENS, BRACKETS };
+
+int getch(void);
+void ungetch(int c);
+void dcl(void);
+void dirdcl(void);
+int gettoken(void);
+
+char buf[BUFSIZE];
+int bufp = 0;
+int tokentype;
+char token[MAXTOKEN];
+char name[MAXTOKEN];
+char datatype[MAXTOKEN];
+char out[1000];
+
+int main(int argc, const char *argv[])
+{
+	while(gettoken() != EOF) {
+		strcpy(datatype, token);
+		out[0] = '\0';
+		dcl();
+		if(tokentype != '\n')
+			printf("syntax error\n");
+		printf("%s: %s %s",name, out, datatype);
+	}
+	return 0;
+}
+
+int gettoken(void)
+{
+	int c, getch(void);
+	void ungetch(int);
+	char *p = token;
+
+	while ((c = getch()) == ' ' || c == '\t')
+		;
+	if (c == '(') {
+		if ((c = getch()) == ')') {
+			strcpy(token, "()");
+			return tokentype = PARENS;
+		} else {
+			ungetch(c);
+			return tokentype = '(';
+		}
+	} else if (c == '[') {
+		for (*p++ = c; (*p++ = getch()) != ']'; )
+			;
+		*p = '\0';
+		return tokentype = BRACKETS;
+	} else if (isalpha(c)) {
+		for (*p++ = c; isalnum(c = getch()); )
+			*p++ = c;
+		*p = '\0';
+		ungetch(c);
+		return tokentype = NAME;
+	} else
+		return tokentype = c;
+}
+
 void dcl(void)
 {
 	int ns;
 
-	for (ns = 0; gettoket() == '*'; )
+	for (ns = 0; gettoken() == '*'; )
 		ns++;
 	dirdcl();
-	while ( ns-- > 0)
-		strcat(out, "pointer to");
+	while (ns-- > 0)
+		strcat(out, " pointer to");
 }
 
 void dirdcl(void)
@@ -14,19 +81,33 @@ void dirdcl(void)
 	int type;
 
 	if (tokentype == '(') {
-		dcl()
-		if(tokentype != ')')
-			printf("Error: missing ) \n");
-	}else if (tokentype == NAME){
+		dcl();
+		if (tokentype != ')')
+			printf("Error: missing )\n");
+		} else if (tokentype == NAME)
 		strcpy(name, token);
 	else
 		printf("Error: expected name or (dlc)\n");
 	while ((type=gettoken()) == PARENS || type == BRACKETS)
-		if (typpe == PARENS)
+		if (type == PARENS)
 			strcat(out, "function returning");
 		else {
 			strcat(out, " array");
 			strcat(out, token);
 			strcat(out, " of");
 		}
+}
+
+
+int getch(void)
+{
+	return (bufp > 0) ? buf[--bufp]: getchar();
+}
+
+void ungetch(int c)
+{
+	if(bufp >= BUFSIZE)
+			printf("ungetch: too many characters\n");
+	else
+			buf[bufp++] = c;
 }
