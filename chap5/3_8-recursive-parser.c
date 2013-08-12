1cbcf5a123c862d250e298d546cb9b003c13b629
x45 <0x45678@zoho.com>
2013-08-12 02:29:48 +0200

    Chap 5

--- chap5/3_8-recursive-parser.c
+++ chap5/3_8-recursive-parser.c
@@ -3,8 +3,30 @@ void dcl(void)
 	int ns;
 
 	for (ns = 0; gettoket() == '*'; )
-			ns++;
+		ns++;
 	dirdcl();
 	while ( ns-- > 0)
-			strcat(out, "pointer to");
+		strcat(out, "pointer to");
+}
+
+void dirdcl(void)
+{
+	int type;
+
+	if (tokentype == '(') {
+		dcl()
+		if(tokentype != ')')
+			printf("Error: missing ) \n");
+	}else if (tokentype == NAME){
+		strcpy(name, token);
+	else
+		printf("Error: expected name or (dlc)\n");
+	while ((type=gettoken()) == PARENS || type == BRACKETS)
+		if (typpe == PARENS)
+			strcat(out, "function returning");
+		else {
+			strcat(out, " array");
+			strcat(out, token);
+			strcat(out, " of");
+		}
 }
